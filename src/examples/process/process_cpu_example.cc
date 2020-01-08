99561668a41441e0f5e08511924bdf03bfd4dc31
bingoli <bingoli2014@qq.com>
2020-01-09 00:16:21 +0800

    增加主消息循环示例

--- src/examples/process/process_cpu_example.cc
+++ src/examples/process/process_cpu_example.cc
@@ -2,16 +2,18 @@
 #include <string>
 #include <windows.h>
 
+#include "base/at_exit.h"
+#include "base/command_line.h"
+#include "base/task/post_task.h"
 #include "base/process/process_metrics.h"
 #include "base/process/process.h"
+#include "base/run_loop.h"
+#include "base/task_runner.h"
+#include "base/task/thread_pool/thread_pool_instance.h"
+#include "base/test/task_environment.h"
+#include "base/test/test_timeouts.h"
 
-int main(int argc, char** argv) {
-  
-  int process_id = 0;
-  if (argc >= 2) {
-    process_id = std::stoi(std::string(argv[1]));
-  }
-
+void GetProcessCpuUsage(int process_id) {
   std::unique_ptr<base::ProcessMetrics> metrics;
   if (process_id <= 0) {
     metrics = base::ProcessMetrics::CreateCurrentProcessMetrics();
@@ -25,6 +27,30 @@ int main(int argc, char** argv) {
     std::cout << cpu_usage << std::endl;
     Sleep(1000);
   }
+}
+
+
+int main(int argc, char** argv) {
+  
+  int process_id = 0;
+  if (argc >= 2) {
+    process_id = std::stoi(std::string(argv[1]));
+  }
+
+  base::CommandLine::Init(argc, argv);
+
+  base::AtExitManager at_exit;
+
+  TestTimeouts::Initialize();
+
+  base::test::TaskEnvironment task_environment(
+      base::test::TaskEnvironment::MainThreadType::UI);
+
+  scoped_refptr<base::TaskRunner> task_runner = base::CreateTaskRunner({base::ThreadPool()});
+  task_runner->PostTask(FROM_HERE, base::BindOnce(GetProcessCpuUsage, process_id));
+
+  base::RunLoop run_loop;
+  run_loop.Run();
 
   return 0;
 }
